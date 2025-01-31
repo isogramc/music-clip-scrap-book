101020fb8c9f788d27050643477eafed79af45fa
Paulo Prado <paulroar@hotmail.com>
2025-01-31 14:40:26 +0000

    Sign up, Login features, basic Css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import ProfilePage from './pages/ProfilePage';
-// import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 // import AboutPage from './pages/AboutPage';
 // import ContactPage from './pages/ContactPage';
 import './styles/global.css';
@@ -13,8 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/profile" element={<ProfilePage />} />
-        {/* <Route path="/login" element={<LoginPage />} />
-        <Route path="/about" element={<AboutPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        {/*<Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />*/}
       </Routes>
     </Router>
