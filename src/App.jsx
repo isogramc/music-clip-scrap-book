47852ec3fac3b8ac093b17d4a1dd8b7e94a54149
theresajvrensburg <theresa@isogramconsulting.com>
2025-01-31 15:45:52 +0100

    keyboard changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 
 import './App.css'
 import { Note, Scale } from "tonal";
-import * as Tone from "tone";
 import Keyboard from './Components/Keyboard'
 
 function App() {
-    const key = Scale.get("C major").notes;
-    //create a synth and connect it to the main output (your speakers)
-
-    function handleClick(){
-        
-        const synth = new Tone.Synth().toDestination();
-
-        //play a middle 'C' for the duration of an 8th note
-        const now = Tone.now();
-        
-        // trigger the attack immediately
-        synth.triggerAttackRelease("C4", "8n", now);
-    }
+  const key = Scale.get("C major").notes;
    
   return (
-
     <div className="visuals">
-        <button onClick={handleClick}>C</button>
-        {key}
      <Keyboard/>
     </div>
   )
