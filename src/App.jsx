391eb6871b782a17c8fabe6903090b0012b4f8ed
theresajvrensburg <theresa@isogramconsulting.com>
2025-02-08 14:58:09 +0100

    ffmpg

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,6 @@ function App() {
   }
 
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route
@@ -58,7 +57,6 @@ function App() {
         {/*<Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />*/}
       </Routes>
-    </Router>
   );
 }
 
